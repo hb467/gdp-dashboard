641657ec44b2ae6e127e2d8b0a6c1b28873ba80b
hb467 <baykarahuseyin@yahoo.com>
2024-12-03 16:52:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ with header_container:
     )
 
 with button_container:
-    st.markdown("<div style='margin-top: 20px; background-color: skyblue; padding: 20px;'>", unsafe_allow_html=True)    col1, col_mid, col2 = st.columns([1, 6, 1])
+    st.markdown("<div style='margin-top: 20px; background-color: skyblue; padding: 20px;'>", unsafe_allow_html=True)
+    col1, col_mid, col2 = st.columns([1, 6, 1])
     with col1:
         if st.button("Eingabe der Schicht"):
             st.info("Eingabeformular ist oben verfügbar.")
