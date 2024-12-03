aa349f59b742f1e6a4a6f5326825af6508f4dc6f
hb467 <baykarahuseyin@yahoo.com>
2024-12-03 16:57:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,22 +36,22 @@ with header_container:
 
 with button_container:
     col1, col_mid, col2 = st.columns([1, 6, 1])
-    col1, col_mid, col2 = st.columns([1, 6, 1])
     with col1:
-                if st.button("Eingabe der Schicht"):
-            st.info("Eingabeformular ist oben verfügbar.")
+    if st.button("Eingabe der Schicht"):
+        st.info("Eingabeformular ist oben verfügbar.")
     with col_mid:
         st.markdown("<h5 style='text-align: center; margin: 0;'>Schichtübersicht</h5>", unsafe_allow_html=True)
     with col2:
-        if st.button("Letzten Eintrag löschen"):
-                        if not df.empty:
-                df = df.iloc[:-1]
-                st.warning("Der letzte Eintrag wurde gelöscht.")
-            else:
-                st.warning("Es gibt keine Einträge zum Löschen.")
+    if st.button("Letzten Eintrag löschen"):
+        if not df.empty:
+            df = df.iloc[:-1]
+            st.warning("Der letzte Eintrag wurde gelöscht.")
+        else:
+            st.warning("Es gibt keine Einträge zum Löschen.")
 
 
 
 with table_container:
     # Display Table with Data Entries
     st.dataframe(df, use_container_width=True)
+
